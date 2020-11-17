7534a80bd21b91488f540c9912508c1550635113
Felipe Marcelino <felipeglicerio@gmarcelino.com>
2020-11-17 17:56:41 -0300

    FInalizando

---
+++ submissao.py
@@ -0,0 +1,36 @@
+import sys
+import os.path
+import requests
+
+
+FILE_PATH = 'equipe_137423741.zip'
+URL = 'https://hackathon-batalha-final.s3.amazonaws.com/'
+POST_DATA = {
+    'key': FILE_PATH,
+    'AWSAccessKeyId': 'AKIA6HWHFK26FN543IED',
+    'policy': 'eyJleHBpcmF0aW9uIjogIjIwMjAtMTEtMThUMDI6NTk6MDBaIiwgImNvbmRpdGlvbnMiOiBbeyJidWNrZXQiOiAiaGFja2F0aG9uLWJhdGFsaGEtZmluYWwifSwgeyJrZXkiOiAiZXF1aXBlXzEzNzQyMzc0MS56aXAifV19',
+    'signature': 'zRSPj7riRTvFsxm1XcQNaYIJUpw='
+}
+
+
+def check_file_exists(file_path):
+    if not os.path.isfile(file_path):
+        print('ERRO: Arquivo %s não encontrado.' % FILE_PATH)
+        sys.exit(0)
+
+
+def upload_file(file_path):
+    with open(file_path, 'rb') as f:
+        files = {'file': (file_path, f)}
+        http_response = requests.post(URL, data=POST_DATA, files=files)
+    return http_response.status_code
+
+
+if __name__ == '__main__':
+    check_file_exists(FILE_PATH)
+    status_code = upload_file(FILE_PATH)
+
+    if status_code == 204:
+        print('Arquivo submetido com sucesso!')
+    else:
+        print('Falha ao submeter o arquivo. HTTP Status Code %s' % status_code)
